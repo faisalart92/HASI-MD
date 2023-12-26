03d2799cda45a808f1b63b003853d1386b89e2e9
faisalart92 <154861324+faisalart92@users.noreply.github.com>
2023-12-26 20:09:35 +0500

    Update config.js

--- config.js
+++ config.js
@@ -3,24 +3,24 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER.split(",")
+global.owner = process.env.OWNER_NUMBER.split("faisal,")
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://mohsin:mohsin@cluster0.iauaztt.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
-global.email = 'infiniteytff@gmail.com'
+global.email = 'fa9228825@gmail.com'
 global.github = 'https://github.com/chhaseeb47/HASI-MD'
 global.location = 'Pakistan'
 global.gurl = 'https://instagram.com/_' // add your username
-global.sudo = process.env.SUDO || '923407472645'
-global.devs = '923407472645';
+global.sudo = process.env.SUDO || '923446302482'
+global.devs = '923446302482';
 global.website = 'https://github.com/chhaseeb47/HASI-MD' // Hello world
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/ea5cfd778d2004c72d965.jpg'
 module.exports = {
-  botname:   process.env.BOT_NAME === undefined ? 'HASI-MD' : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? '𝗖𝗛 𝗛𝗔𝗦𝗘𝗘𝗕' : process.env.OWNER_NAME,
+  botname:   process.env.BOT_NAME === undefined ? 'FAISAL-FA' : process.env.BOT_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? 'Faisal' : process.env.OWNER_NAME,
   sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,
-  author:  process.env.PACK_AUTHER.split(";")[0] === undefined ? '𝗖𝗛 𝗛𝗔𝗦𝗘𝗘𝗕' : process.env.PACK_AUTHER.split(";")[0],
+  author:  process.env.PACK_AUTHER.split(";")[0] === undefined ? 'faisal' : process.env.PACK_AUTHER.split(";")[0],
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
-  packname:  process.env.PACK_NAME.split(";")[1] === undefined ? 'HASI-MD' : process.env.PACK_NAME.split(";")[1],
+  packname:  process.env.PACK_NAME.split(";")[1] === undefined ? 'FAISAL-FA' : process.env.PACK_NAME.split(";")[1],
   autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nigga' : process.env.ANTI_BAD_WORD,
   alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
